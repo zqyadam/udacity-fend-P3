0f55f25a745b4421f234003e406484f379d002ff
zqyadam <zqyadam@qq.com>
2018-04-22 18:03:21 +0800

    index.html适配425px

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,9 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8080 // Change this to your server port
-    return `http://127.0.0.1:${port}/data/restaurants.json`;
+    const remote = false;
+    // return `http://${remote?'192.168.3.100':'127.0.0.1'}:${port}/data/restaurants.json`;
+    return `/data/restaurants.json`;
   }
 
   /**
