349ccc29fc92d4d1f26ce74296dbd1dfd712ce84
zqyadam <zqyadam@qq.com>
2018-04-23 21:38:33 +0800

    test

--- js/main.js
+++ js/main.js
@@ -143,22 +143,27 @@ createRestaurantHTML = (restaurant) => {
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
+  const container = document.createElement('div');
+  container.className = 'restaurant-detail';
+  li.append(container);
+
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
-  li.append(name);
+  // li.append(name);
+  container.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
-  li.append(neighborhood);
+  container.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
-  li.append(address);
+  container.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  li.append(more)
+  container.append(more)
 
   return li
 }
